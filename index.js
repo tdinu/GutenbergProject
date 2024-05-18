03afadca3ff34be2f499601efe11464ff29f061e
tdinu <dinutitiana@yahoo.com>
2024-05-18 18:14:02 +0200

    added basic structure to index file

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
-/**
- * @format
- */
+import React from 'react';
+import { AppRegistry, SafeAreaView } from 'react-native';
+import { name as appName } from './app.json';
 
-import {AppRegistry} from 'react-native';
-import App from './App';
-import {name as appName} from './app.json';
+const MyApp = () => {
+  return (
+    // eslint-disable-next-line react-native/no-inline-styles
+    <SafeAreaView style={{ flex: 1 }} />
+  );
+};
 
-AppRegistry.registerComponent(appName, () => App);
+AppRegistry.registerComponent(appName, () => MyApp);
