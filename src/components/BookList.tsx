bd52d52c80c52f3380a40340ae9ad0b8430a4c5c
tdinu <dinutitiana@yahoo.com>
2024-05-19 08:54:21 +0200

    start adding dummy data for the list

--- src/components/BookList.tsx
+++ src/components/BookList.tsx
@@ -1,13 +1,104 @@
-import { StyleSheet, Text, View, ScrollView } from 'react-native';
+import {
+  StyleSheet,
+  Text,
+  View,
+  ScrollView,
+  ActivityIndicator,
+} from 'react-native';
+import React, { useState, useEffect } from 'react';
 
 const BookList = () => {
+  let [isLoading, setIsLoading] = useState(true);
+  let [error, setError] = useState();
+  let [response, setResponse] = useState<unknown>();
+
+  useEffect(() => {
+    setIsLoading(false);
+    setResponse([
+      {
+        id: 84,
+        title: 'Frankenstein; Or, The Modern Prometheus',
+        authors: [
+          {
+            name: 'Shelley, Mary Wollstonecraft',
+          },
+        ],
+        bookshelves: ['Gothic Fiction'],
+        formats: {
+          'image/jpeg':
+            'https://www.gutenberg.org/cache/epub/84/pg84.cover.medium.jpg',
+        },
+      },
+      {
+        id: 1342,
+        title: 'Pride and Prejudice',
+        authors: [
+          {
+            name: 'Austen, Jane',
+          },
+        ],
+        bookshelves: ['Harvard Classics'],
+        formats: {
+          'image/jpeg':
+            'https://www.gutenberg.org/cache/epub/1342/pg1342.cover.medium.jpg',
+        },
+      },
+      {
+        id: 2701,
+        title: 'Moby Dick; Or, The Whale',
+        authors: [
+          {
+            name: 'Melville, Herman',
+          },
+        ],
+        bookshelves: ['Best Books Ever Listings'],
+        formats: {
+          'image/jpeg':
+            'https://www.gutenberg.org/cache/epub/2701/pg2701.cover.medium.jpg',
+        },
+      },
+      {
+        id: 1259,
+        title: 'Twenty years after',
+        authors: [
+          {
+            name: 'Dumas, Alexandre',
+          },
+          {
+            name: 'Maquet, Auguste',
+          },
+        ],
+        bookshelves: ['Adventure', 'Historical Fiction', 'Movie Books'],
+        formats: {
+          'image/jpeg':
+            'https://www.gutenberg.org/cache/epub/1259/pg1259.cover.medium.jpg',
+        },
+      },
+    ]);
+    setError(undefined);
+  }, []);
+
+  const getContent = () => {
+    if (isLoading) {
+      return <ActivityIndicator size="large" />;
+    }
+
+    if (error) {
+      return <Text>{error}</Text>;
+    }
+
+    if (response) {
+      console.warn(response);
+    }
+  };
+
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Books List</Text>
       <ScrollView
         contentInsetAdjustmentBehavior="automatic"
         style={styles.content}>
-        <Text>Render List</Text>
+        {getContent()}
       </ScrollView>
     </View>
   );
