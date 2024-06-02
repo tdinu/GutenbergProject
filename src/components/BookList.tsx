2554cadd1a022561919257c5ca47705c03bc5c36
tdinu <dinutitiana@yahoo.com>
2024-06-02 21:00:26 +0200

    implement api request

--- src/components/BookList.tsx
+++ src/components/BookList.tsx
@@ -8,68 +8,7 @@ import {
 import React, { useState, useEffect } from 'react';
 import { Book } from '../types';
 import BookDetails from './BookDetails';
-
-const dummyData: Book[] = [
-  {
-    id: 84,
-    title: 'Frankenstein; Or, The Modern Prometheus',
-    authors: [
-      {
-        name: 'Shelley, Mary Wollstonecraft',
-      },
-    ],
-    bookshelves: ['Gothic Fiction'],
-    formats: {
-      'image/jpeg':
-        'https://www.gutenberg.org/cache/epub/84/pg84.cover.medium.jpg',
-    },
-  },
-  {
-    id: 1342,
-    title: 'Pride and Prejudice',
-    authors: [
-      {
-        name: 'Austen, Jane',
-      },
-    ],
-    bookshelves: ['Harvard Classics'],
-    formats: {
-      'image/jpeg':
-        'https://www.gutenberg.org/cache/epub/1342/pg1342.cover.medium.jpg',
-    },
-  },
-  {
-    id: 2701,
-    title: 'Moby Dick; Or, The Whale',
-    authors: [
-      {
-        name: 'Melville, Herman',
-      },
-    ],
-    bookshelves: ['Best Books Ever Listings'],
-    formats: {
-      'image/jpeg':
-        'https://www.gutenberg.org/cache/epub/2701/pg2701.cover.medium.jpg',
-    },
-  },
-  {
-    id: 1259,
-    title: 'Twenty years after',
-    authors: [
-      {
-        name: 'Dumas, Alexandre',
-      },
-      {
-        name: 'Maquet, Auguste',
-      },
-    ],
-    bookshelves: ['Adventure', 'Historical Fiction', 'Movie Books'],
-    formats: {
-      'image/jpeg':
-        'https://www.gutenberg.org/cache/epub/1259/pg1259.cover.medium.jpg',
-    },
-  },
-];
+import axios from 'axios';
 
 const BookList = () => {
   let [isLoading, setIsLoading] = useState(true);
@@ -77,45 +16,20 @@ const BookList = () => {
   let [response, setResponse] = useState<Book[]>([]);
 
   useEffect(() => {
-    setIsLoading(false);
-
-    (async () => {
+    const fetchData = async () => {
+      setIsLoading(true);
       try {
-        const success = await getMockData(
-          [...dummyData],
-          'unknown server error',
-          500,
-        );
-        setResponse(success.data);
+        const success = await axios.get('https://gutendex.com/books');
+        setResponse(success.data.results);
       } catch (err) {
         const { message } = err as Error;
         setError(message);
       }
-    })();
-  }, []);
+      setIsLoading(false);
+    };
 
-  const getMockData = async (
-    data: Book[] = [],
-    // eslint-disable-next-line @typescript-eslint/no-shadow
-    error = 'unknown server error',
-    delay = 1000,
-  ) => {
-    return new Promise<{ type: String; data: Book[] }>((resolve, reject) => {
-      setTimeout(() => {
-        if (data) {
-          resolve({
-            type: 'Success',
-            data,
-          });
-        } else {
-          reject({
-            type: 'Error',
-            message: error,
-          });
-        }
-      }, delay);
-    });
-  };
+    fetchData();
+  }, []);
 
   const getContent = () => {
     if (isLoading) {
